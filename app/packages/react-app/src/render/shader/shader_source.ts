d08f37c4066cc9f1f09207ddafd65f5462b2e553
snowapril <sinjihng@gmail.com>
2022-04-26 14:23:00 +0900

    feat(webgl_framework): add common vertex shader source for quad render

---
+++ app/packages/react-app/src/render/shader/shader_source.ts
@@ -0,0 +1,17 @@
+const QUAD_SHADER_SOURCE: string = `
+#version 450 core
+
+precision highp float;
+precision highp int;
+
+layout ( location = 0 ) out VSOUT{
+	vec2 texCoord;
+} vs_out;
+
+void main() {
+	vec2 pos = vec2(gl_VertexID & 1, (gl_VertexID & 2) >> 1) * 2 - 1;
+	vs_out.texCoord = pos.xy * 0.5 + 0.5;
+	gl_Position = vec4(pos.xy, 0, 1);
+}`;
+
+export default QUAD_SHADER_SOURCE;
