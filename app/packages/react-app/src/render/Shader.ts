fafcef7efec7a9ad87a36875f63069a518b77a45
snowapril <sinjihng@gmail.com>
2022-04-26 13:38:34 +0900

    feat(webgl_framework): add initial shader class

---
+++ app/packages/react-app/src/render/Shader.ts
@@ -0,0 +1,26 @@
+class Shader {
+    private gl: WebGL2RenderingContext;
+    private vs: WebGLShader;
+    private fs: WebGLShader;
+
+    constructor(gl: WebGL2RenderingContext, vs_source: string, fs_source: string) {
+        this.gl = gl;
+        this.vs = this.createShader(WebGL2RenderingContext.VERTEX_SHADER, vs_source);
+        this.fs = this.createShader(WebGL2RenderingContext.FRAGMENT_SHADER, fs_source);
+    }
+
+    private createShader = (type: GLenum, source: string): WebGLShader => {
+        let shader: WebGLShader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+        
+        if ( !this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS) ) {
+            let info = this.gl.getShaderInfoLog( shader );
+            throw 'Could not compile WebGL program. \n\n' + info;
+        }
+
+        return shader;
+    }
+};
+
+export default Shader;
