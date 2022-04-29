3da06f8cc78a29e91f4e0a465fbf4dfbf7bffbad
snowapril <sinjihng@gmail.com>
2022-04-29 17:06:27 +0900

    feat(webgl_framework): add shader draw, tryCompile methods

--- app/packages/react-app/src/render/ShaderNFT.ts
+++ app/packages/react-app/src/render/ShaderNFT.ts
@@ -1,24 +1,52 @@
 import Shader from "./Shader";
+import {QUAD_SHADER_SOURCE, SAMPLE_FRAGMENT_SHADER_SOURCE} from "./shader/shader_source"
 
 /**
  * Set of shader and live canvas for rendering ShaderNFT
+ * @todo sustain canvas without crashing when the given shader cannot be compiled
  */
 class ShaderNFT {
     private gl: WebGLRenderingContext;
     private shader: Shader;
-    private canvas : HTMLCanvasElement;
 
     /**
-     * 
      * @param gl WebGL context
-     * @param vs_source source code string of vertex shader
-     * @param fs_source source code string of fragment shader
+     * @param source source code string of fragment shader
      * @param canvas canvas element to be rendered
      */
-    constructor(gl: WebGLRenderingContext, vs_source: string, fs_source: string, canvas: HTMLCanvasElement) {
+    constructor(gl: WebGLRenderingContext, source: string) {
         this.gl = gl;
-        this.canvas = canvas;
-        this.shader = new Shader(gl, vs_source, fs_source);
+        this.shader = new Shader(gl, QUAD_SHADER_SOURCE, source);
+    }
+
+    /**
+     * Draw shader rendering result on the given context
+     * @param gl WebGL context
+     * @param frameCount frame count
+     */
+    public draw = (gl: WebGLRenderingContext, frameCount: number): void => {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        // TODO(snowapril) : gl.viewport(0, 0, canvas.width, canvas.height);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        this.shader.bind();
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    }
+
+    /**
+     * This callback will be passed into editor onChanged event
+     * @param source source code string of fragment shader
+     */
+    public tryCompile = (source: string): void => {
+        try {
+            let newShader: Shader = new Shader(this.gl, QUAD_SHADER_SOURCE, source);
+            // When compile success, destroy original shader and replace with new shader
+            this.shader.destroy();
+            this.shader = newShader;
+        } catch (e) {
+            console.log(e);
+        }
     }
 };
 
