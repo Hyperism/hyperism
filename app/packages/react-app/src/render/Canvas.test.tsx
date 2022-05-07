7649d455b5c6f311e1c81cb313ac1e20d5bb1326
snowapril <sinjihng@gmail.com>
2022-05-07 17:25:13 +0900

    test(dapp_front): replace gl to canvas for using webgl2 context

--- app/packages/react-app/src/render/Canvas.test.tsx
+++ app/packages/react-app/src/render/Canvas.test.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import Canvas from "@my-app/react-app/src/render/Canvas";
 
 test("renders empty red screen using webgl", () => {
-  const draw = (gl: WebGLRenderingContext, frameCount: number) => {
+  const init = (_gl: WebGL2RenderingContext) => { }
+  const draw = (gl: WebGL2RenderingContext, frameCount: number) => {
     gl.clearColor(1, 0, 0, 1);
   }
-  // const { getByText } = render(<Cavnas draw={draw}/>);
+  // const { getByText } = render(<Canvas initCall={init} drawCall={draw}/>);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 });
--- app/packages/react-app/src/render/Shader.test.ts
+++ app/packages/react-app/src/render/Shader.test.ts
@@ -4,7 +4,7 @@ import React, { CanvasHTMLAttributes } from "react";
 import Shader from "@my-app/react-app/src/render/Shader";
 
 test("Shader compiles test", () => {
-  let vs_source = `
+  let vs_source = `#version 300 es
     attribute vec4 a_position;
     attribute vec4 a_color;
     out vec4 v_color;
@@ -13,13 +13,21 @@ test("Shader compiles test", () => {
         v_color = a_color;
     }
   `;
-  let fs_source = `
+  let fs_source = `#version 300 es
     precision mediump float;
     in vec4 v_color;
     void main() {
         gl_FragColor = v_color;
     }
   `;
-  var gl = require("gl")(100, 100);
+
+  const canvas: HTMLCanvasElement = document.createElement("canvas");
+  const gl: WebGL2RenderingContext | null = canvas.getContext("webgl2", { antialias: false});
+
+  if (gl === null) {
+      console.log("WebGL2 not available");
+      return;
+  }
+
   expect(new Shader(gl, vs_source, fs_source));
 });
