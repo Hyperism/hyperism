178ba088011ab0d36769df481628e2d81988dc6b
snowapril <sinjihng@gmail.com>
2022-05-07 16:52:11 +0900

    feat(dapp_front): add interface for passing props to canvas component

--- app/packages/react-app/src/render/Canvas.tsx
+++ app/packages/react-app/src/render/Canvas.tsx
@@ -1,19 +1,31 @@
 import React, {useRef, useEffect} from "react";
 
 /**
+ * @param initCall - initialization function
+ * @param drawCall - draw function
+ */
+interface CanvasProps {
+  initCall: (gl: WebGL2RenderingContext) => void,
+  drawCall: (gl: WebGL2RenderingContext, frameCount: number) => void
+}
+
+/**
  * Canvas where given shader rendered
  * 
  * @param drawCall - main draw loop funtion with webgl rendering context and frame count
  * @returns JSX element
  */
-const Canvas = ({drawCall}: {drawCall: (gl: WebGL2RenderingContext, frameCount: number) => void}): JSX.Element => {
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
+const Canvas = (props: CanvasProps): JSX.Element => {  /* eslint-disable  @typescript-eslint/no-explicit-any */
   const canvasRef : any = useRef(null) 
-
+  const {initCall, drawCall} = props;
   useEffect(() => {
     /* eslint-disable  @typescript-eslint/no-explicit-any */
     const canvas : any = canvasRef.current
     const gl : WebGL2RenderingContext = canvas.getContext('webgl2')
+
+    // Initialize opengl resources before entering drawing loop
+    initCall(gl);
+
     let frameCount = 0
     let animationFrameId : number;
     //Our draw came here
