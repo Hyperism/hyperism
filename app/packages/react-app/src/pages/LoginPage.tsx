1080e80736ed47559d21adb9c8b57056286a814f
snowapril <sinjihng@gmail.com>
2022-05-17 02:02:15 +0900

    feat(vulkan_setup): add login page and send REST API query

---
+++ app/packages/react-app/src/pages/LoginPage.tsx
@@ -0,0 +1,206 @@
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import logo from "../ethereumLogo.png";
+import Button from "@mui/material/Button";
+import { Header, Body, Image, Link } from "../components";
+import Editor from "../components/Editor";
+import { Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright(props: any): JSX.Element {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        Hyperism
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+function LoginPage(): JSX.Element {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const [emailField, setEmailField] = useState<string>("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEmailField(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const [passwordField, setPasswordField] = useState<string>("");
+
+  const handlePasswordChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setPasswordField(event.target.value);
+    console.log(event.target.value);
+  };
+
+  function joinHandler(email: string, password: string) {
+    try {
+      const data = { email: email, password: password };
+      axios
+        .post("http://localhost:3000/loginin", JSON.stringify(data), {
+          headers: {
+            "Content-Type": `application/json`,
+          },
+        })
+        .then((res) => {
+          console.log("signup requset success : " + res);
+          // console.log(axios.defaults.headers.common['Authorization']);
+          // axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+          console.log(localStorage.getItem("user"));
+          localStorage.setItem("user", JSON.stringify(res.data));
+          console.log("Bearer " + res.data.token);
+        })
+        .catch((ex) => {
+          console.log("signup requset fail : " + ex);
+        })
+        .finally(() => {
+          console.log("Signup Request End");
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/800px-Ethereum_logo_translucent.svg.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleEmailChange}
+                value={emailField}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={handlePasswordChange}
+                value={passwordField}
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={() => {
+                  joinHandler(emailField, passwordField);
+                }}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default LoginPage;
