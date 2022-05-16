0b6f3ab2e3a14b1399bada5af3c5d5d8d4c41247
snowapril <sinjihng@gmail.com>
2022-05-17 02:01:44 +0900

    dependency(dapp_register): remove graphql as we deploy self-hosted REST API & db

--- app/packages/react-app/src/index.tsx
+++ app/packages/react-app/src/index.tsx
@@ -1,6 +1,5 @@
 import "./index.css";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { DAppProvider, Mainnet } from "@usedapp/core";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -16,19 +15,10 @@ const config = {
   },
 }
 
-// You should replace this url with your own and put it into a .env file
-// See all subgraphs: https://thegraph.com/explorer/
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <DAppProvider config={config}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
+      <App />
     </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root"),
