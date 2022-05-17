9d14ae130c190881f7acea6a63fcb28e9d41e269
snowapril <sinjihng@gmail.com>
2022-05-17 22:03:59 +0900

    feat(dapp_register): add npm hardhat mumbai testnet configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,41 @@
 require("@nomiclabs/hardhat-waffle");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
 module.exports = {
-  solidity: "0.8.2",
-};
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./app/packages/contracts/src"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
