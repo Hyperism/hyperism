a6a0e38bd642bcfe8fc31148b79d986d91e3725d
snowapril <sinjihng@gmail.com>
2022-05-18 00:40:40 +0900

    feat(dapp_register): comment out mumbai test net config

--- app/packages/react-app/src/index.tsx
+++ app/packages/react-app/src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import { DAppProvider, Mumbai } from "@usedapp/core";
+import { DAppProvider, Mainnet } from "@usedapp/core";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -9,11 +9,11 @@ import App from "./App";
 // Change this to your own Infura project id: https://infura.io/register
 const INFURA_PROJECT_ID = "defba93b47f748f09fcead8282b9e58e";
 const config = {
-  readOnlyChainId: Mumbai.chainId,
+  readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
-    [Mumbai.chainId]: "https://ropsten.infura.io/v3/",
+    [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
   },
-};
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,5 +21,31 @@ ReactDOM.render(
       <App />
     </DAppProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
+
+// import "./index.css";
+
+// import { DAppProvider, Mumbai } from "@usedapp/core";
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// // Change this to your own Infura project id: https://infura.io/register
+// const INFURA_PROJECT_ID = "defba93b47f748f09fcead8282b9e58e";
+// const config = {
+//   readOnlyChainId: Mumbai.chainId,
+//   readOnlyUrls: {
+//     [Mumbai.chainId]: "https://ropsten.infura.io/v3/",
+//   },
+// };
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <DAppProvider config={config}>
+//       <App />
+//     </DAppProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
