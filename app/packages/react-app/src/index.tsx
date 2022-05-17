c9f16684f21e19b23c0086aa197717244e7a7b7e
snowapril <sinjihng@gmail.com>
2022-05-17 22:07:47 +0900

    feat(dapp_register): modify usedapp index with mumbai test-net

--- app/packages/react-app/src/index.tsx
+++ app/packages/react-app/src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import { DAppProvider, Mainnet } from "@usedapp/core";
+import { DAppProvider, Mumbai } from "@usedapp/core";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -9,11 +9,11 @@ import App from "./App";
 // Change this to your own Infura project id: https://infura.io/register
 const INFURA_PROJECT_ID = "defba93b47f748f09fcead8282b9e58e";
 const config = {
-  readOnlyChainId: Mainnet.chainId,
+  readOnlyChainId: Mumbai.chainId,
   readOnlyUrls: {
-    [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
+    [Mumbai.chainId]: "https://ropsten.infura.io/v3/",
   },
-}
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,5 +21,5 @@ ReactDOM.render(
       <App />
     </DAppProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
