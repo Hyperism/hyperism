bd992ea70cabe8afe3573b68210ab82152b15652
snowapril <sinjihng@gmail.com>
2022-05-18 00:41:30 +0900

    feat(dapp_register): add link to signup page

--- app/packages/react-app/src/pages/LoginPage.tsx
+++ app/packages/react-app/src/pages/LoginPage.tsx
@@ -190,7 +190,7 @@ function LoginPage(): JSX.Element {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
