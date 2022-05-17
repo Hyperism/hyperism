bd613f97d1a6db820ed52fe1a8674c8704787b29
snowapril <sinjihng@gmail.com>
2022-05-17 22:06:24 +0900

    feat(dapp_register): add redirect component if not login

--- app/packages/react-app/src/App.tsx
+++ app/packages/react-app/src/App.tsx
@@ -2,24 +2,75 @@ import ResponsiveAppBar from "./components/AppBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navigate } from "react-router";
 import MainPage from "./pages/MainPage";
-import EditorPage from "./pages/EditorPage"
+import EditorPage from "./pages/EditorPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import ExplorerPage from "./pages/ExplorerPage";
 
 function App(): JSX.Element {
-
-  const bUserLogin = localStorage.getItem("user") != null;
-
   return (
     <BrowserRouter>
-      <div style={{background: 'inherit' }}>
+      <div style={{ background: "inherit" }}>
         <ResponsiveAppBar />
         <Routes>
           <Route path="/" element={MainPage()} />
-          <Route path="/editor" element={ bUserLogin ? EditorPage() : <Navigate to="/"/>} />
-          <Route path="/main" element={<Navigate to="/"/>} />
-          <Route path="/signup" element={SignUpPage()} />
-          <Route path="/login" element={LoginPage()} />
+          <Route
+            path="/editor"
+            element={
+              localStorage.getItem("user") ? (
+                <EditorPage />
+              ) : (
+                <Navigate
+                  to={{
+                    pathname: "/login",
+                  }}
+                />
+              )
+            }
+          />
+          <Route path="/main" element={<Navigate to="/" />} />
+          <Route
+            path="/signup"
+            element={
+              localStorage.getItem("user") ? (
+                <SignUpPage />
+              ) : (
+                <Navigate
+                  to={{
+                    pathname: "/login",
+                  }}
+                />
+              )
+            }
+          />
+          <Route
+            path="/explore"
+            element={
+              localStorage.getItem("user") ? (
+                <ExplorerPage />
+              ) : (
+                <Navigate
+                  to={{
+                    pathname: "/login",
+                  }}
+                />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              localStorage.getItem("user") ? (
+                <Navigate
+                  to={{
+                    pathname: "/main",
+                  }}
+                />
+              ) : (
+                <LoginPage />
+              )
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
