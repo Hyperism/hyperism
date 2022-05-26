c92ed7ec2dfeac3aaf640f9870815d1d3c1f1266
snowapril <sinjihng@gmail.com>
2022-05-27 00:17:15 +0900

    feat(main): itearting shader codes and rendering, add nft owner updating

--- app/packages/react-app/src/pages/ExplorePage.tsx
+++ app/packages/react-app/src/pages/ExplorePage.tsx
@@ -5,6 +5,8 @@ import { UserLoginInfo, MetadataInfo, ShaderQueryInfo } from "./Interfaces";
 import axios from "axios";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
 import {
   getWallet,
   getMstbyId,
@@ -25,51 +27,48 @@ function ExplorePage(): JSX.Element {
     Map<string, ShaderQueryInfo>
   >(() => new Map());
 
-  const tradeHandler = async (metaId: string, price: number) => {
-    const mstaddr = await getMstbyId(metaId)
-    const tstaddr = await getTstbyMst(mstaddr)
-    const wallet = getWallet();
-  
-    onPurchase(wallet, tstaddr, mstaddr, 1, price)
-    .then(() => {
-      // Modify owner of this meta info with new owner
-    })
-    .catch((e) => {
-      console.log(e);
-    })
-    .finally(() => {
-      "NFT Trading Success"
-    })
+  const tradeHandler = (metaId: string, price: number) => () => {
+    /* eslint-disable   @typescript-eslint/no-non-null-assertion */
+    const userObj: UserLoginInfo = JSON.parse(localStorage.getItem("user")!);
 
-    // change owner of the shader
-  };
-
-  const queryShaderCode = (token: string, id: string) => {
-    try {
-      console.log("queryShaderCode token : {}", token);
-      axios
-        .post(`http://localhost:3000/api/meta/getshader/${id}`, {
-          headers: {
-            "Content-Type": `application/json`,
-            Authorization: token,
-          },
+    getWallet().then((wallet) => {
+      const mstaddr = getMstbyId(metaId);
+      const tstaddr = getTstbyMst(mstaddr);
+      onPurchase(wallet, tstaddr, mstaddr, 1, price)
+        .then(() => {
+          // Modify owner of this meta info with new owner
+          const form = new FormData();
+          form.append("owner", userObj.user.username);
+          axios
+            .put(`http://localhost:3000/api/meta/update/${metaId}`, form, {
+              headers: {
+                "Content-Type": `application/json`,
+                Authorization: `${userObj.token}`,
+              },
+            })
+            .then((res) => {
+              console.log(
+                "GET api/meta Request success : " + JSON.stringify(res.data)
+              );
+              if (res.data.data != null) {
+                setShaderNFTs(res.data.data);
+              }
+            })
+            .catch((ex) => {
+              console.log("GET api/meta Request fail : " + ex);
+            })
+            .finally(() => {
+              console.log("GET api/meta Request End");
+            });
         })
-        .then((res) => {
-          console.log(
-            "POST api/meta/getshader Request success : " +
-              JSON.stringify(res.data)
-          );
-          setShaderCodes(shaderCodes.set(id, res.data));
-        })
-        .catch((ex) => {
-          console.log(`http://localhost:3000/api/meta/getshader/${id}` + ex);
+        .catch((e) => {
+          console.log(e);
         })
         .finally(() => {
-          console.log("POST api/meta Request End");
+          "NFT Trading Success";
         });
-    } catch (e) {
-      console.log(e);
-    }
+    });
+    // change owner of the shader
   };
 
   React.useEffect(() => {
@@ -91,11 +90,6 @@ function ExplorePage(): JSX.Element {
           );
           if (res.data.data != null) {
             setShaderNFTs(res.data.data);
-            console.log(typeof res.data.data);
-            for (let idx = 0; idx < res.data.data.length; idx++) {
-              console.log("ShaderNFT : {}", res.data.data[idx]);
-              queryShaderCode(userObj.token, res.data.data[idx]._id);
-            }
           }
         })
         .catch((ex) => {
@@ -109,49 +103,87 @@ function ExplorePage(): JSX.Element {
     }
   }, []);
 
+  React.useEffect(() => {
+    /* eslint-disable   @typescript-eslint/no-non-null-assertion */
+    const userObj: UserLoginInfo = JSON.parse(localStorage.getItem("user")!);
+    console.log(userObj);
+
+    shaderNFTs.forEach((metaInfo) => {
+      try {
+        console.log("queryShaderCode token : {}", userObj.token);
+        axios
+          .get(`http://localhost:3000/api/meta/getshader/${metaInfo._id}`, {
+            headers: {
+              "Content-Type": `application/json`,
+              Authorization: `${userObj.token}`,
+            },
+          })
+          .then((res) => {
+            console.log(
+              "GET api/meta/getshader Request success : " +
+                JSON.stringify(res.data)
+            );
+            console.log("Shader Code : {}", shaderCodes);
+            setShaderCodes(shaderCodes.set(metaInfo._id, res.data));
+          })
+          .catch((ex) => {
+            console.log(
+              `http://localhost:3000/api/meta/getshader/${metaInfo._id}` + ex
+            );
+          })
+          .finally(() => {
+            console.log("GET api/meta Request End");
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  }, [shaderNFTs]);
+
   return (
     <Container sx={{ bgcolor: "#282C34" }}>
       <Container sx={{ py: 8 }} maxWidth="md">
         <Grid container spacing={4}>
-          <Grid item key={0} xs={12}></Grid>
+          <Grid item key={0} xs={12}>
+            {shaderNFTs.map((shaderInfo) => {
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Editor
+                  defaultShader={shaderCodes.get(shaderInfo._id)!.shader}
+                />
+              </Card>;
+              <FormControl fullWidth sx={{ m: 4 }}>
+                Owner : ${shaderInfo.owner}
+                Minter: ${shaderInfo.minter}
+                Price : ${shaderInfo.price} ether
+                <Button
+                  key={"Buy"}
+                  onClick={tradeHandler(shaderInfo._id, shaderInfo.price)}
+                  sx={{
+                    my: 2,
+                    bgcolor: "#161B22",
+                    color: "white",
+                    display: "block",
+                  }}
+                >
+                  {"Buy"}
+                </Button>
+              </FormControl>;
+              <FormControl fullWidth sx={{ m: 4 }}>
+                Title : ${shaderInfo.title}
+                Description: ${shaderInfo.description}
+              </FormControl>;
+            })}
+          </Grid>
         </Grid>
       </Container>
     </Container>
   );
 }
 
-// {itemData.map((item) => (
-//   <ImageListItem key={item.img}>
-//     <img
-//       src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-//       srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-//       alt={item.title}
-//       loading="lazy"
-//     />
-//   </ImageListItem>
-// ))}
-
 export default ExplorePage;
-
-// {shaderNFTs.map((shaderInfo) => {
-//   <Card
-//     sx={{
-//       height: "100%",
-//       display: "flex",
-//       flexDirection: "column",
-//     }}
-//   >
-//     <Editor
-//       defaultShader={shaderCodes.get(shaderInfo._id)!.shader}
-//     />
-//   </Card>;
-// <FormControl fullWidth sx={{ m: 1 }}>
-//   <Button
-//     key={"Mint"}
-//     onClick={tradeHandler}
-//     sx={{ my: 2, bgcolor: "#161B22", color: "white", display: "block" }}
-//   >
-//     {"Mint"}
-//   </Button>
-// </FormControl>;
-// })}
