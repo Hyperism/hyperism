eb70c36333da03ce8f23c44072dac3bafeaffbcc
snowapril <sinjihng@gmail.com>
2022-05-26 22:38:38 +0900

    feat(main): add network, paths config for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,33 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.2",
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./app/packages/contracts/src"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
 
 // require("@nomiclabs/hardhat-waffle");
